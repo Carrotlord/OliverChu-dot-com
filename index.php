59917193184f9880f10a76b814ab7aede9591493
Oliver Chu <j.oliverchu@berkeley.edu>
2013-01-07 06:11:18 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Welcome to the source code for OliverChu.com!
+             This HTML source was all written by
+             Mr. Jiangcheng Oliver Chu, by hand.           -->
+        <title>OliverChu.com</title>
+        <!-- Global CSS -->
+        <link rel="stylesheet" type="text/css" href="global.css" />
+        <!-- Global JavaScript -->
+        <script type="text/javascript" src="global.js"></script>
+        <!-- Favicon (Website icon) -->
+        <link rel="shortcut icon" href="img/favicon.ico" />
+    </head>
+
+    <body class="main smallindent">
+        <h1>
+            <a href="webOS.php">main</a>.OliverChu
+        </h1>
+        <div class="center">
+            <img src="http://thezerothblog.files.wordpress.com/2012/12/jiangchengoliverchu.png" alt="Oliver Chu" class="fancy" /><br />
+            <span class="hefty important">Who is Oliver Chu?</span>
+        </div>
+        <div class="halfbreak"></div>
+        <table class="centerobject" width="80%"><tr><td>
+        Jiangcheng Chu, nicknamed Oliver Chu by his loving family, is a computer scientist and software
+        engineer. He enjoys programming in various computer languages and working on side projects. He
+        also likes Ultimate Frisbee, good food, and friendly people.
+        <div style="height:16px;">&nbsp;</div>
+        
+        <table class="centerobject" cellspacing="4" cellpadding="8">
+        <tr>
+            <td style="vertical-align:top;text-align:left;">
+                <span class="important">Info</span><br />
+                <a href="aboutoliver.php">Read More about Oliver.</a><br />
+                <a href="philosophy.php">My Philosophy &amp; Rules of Life</a><br />
+                <a href="http://pastebin.com/KPk3PVVR" target="_blank">Example Technical Job Interview</a>&nbsp;&nbsp;&nbsp;
+            </td>
+
+            <td style="vertical-align:top;text-align:left;">
+                <span class="important">Creations</span><br />
+                <a href="proj.php">View Oliver&#39;s Projects</a><br />
+                <a href="http://mint.oliverchu.com/">The Mint Programming Language</a><br />
+                <a href="http://www.cavestory.org/forums/index.php?/topic/4562-protoflight/" target="_blank">ProtoFlight - A Free Java Game</a>
+            </td>
+        </tr>
+        <tr>
+            <td style="vertical-align:top;text-align:left;">
+                <span class="important">Contact</span><br />
+                <a href="http://www.linkedin.com/in/jiangchengoliverchu" target="_blank">
+                LinkedIn Profile</a><br />
+                <a href="resume.php" target="_blank">
+                Resume</a>&nbsp;&nbsp;&nbsp;
+                <br />
+                <a href="card.php" target="_blank">Business Card</a>
+            </td>
+
+            <td style="vertical-align:top;text-align:left;">
+                <span class="important">OllieNET</span><br />
+                <a href="http://game.oliverchu.com/register.php">Sign Up</a><br />
+                <a href="http://hive.oliverchu.com">Edit Code Online</a><br />
+                <a href="http://www.oliverchu.com/onedollarapps.php">$1 App Store</a>
+            </td>
+        </tr>
+        </table>
+<br /><br />
+<div class="center">
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/legalcode" target="_blank"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">This domain and website</span> is licensed under a <br /><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/legalcode" target="_blank">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License</a>.
+</div><br /><br /><br />
+        <!-- End of Content -->
+    </body>
+
+</html>
